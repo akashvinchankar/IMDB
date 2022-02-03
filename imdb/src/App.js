3e99bdfdab257f17b40d4a3625d097e9ae8e9ae7
akashvinchankar <86399324+akashvinchankar@users.noreply.github.com>
2022-02-03 22:42:03 +0530

    react import

---
+++ imdb/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar';
+function App() {
+  return (
+    <>
+    <h1>Hello Akash!</h1>
+    {/* <h2>Navbar</h2> */}
+    <NavBar></NavBar>
+    <h2>Banner</h2>
+    <h2>Trending</h2>
+    <h2>Pagination</h2> 
+    </>
+  );
+}
+
+export default App;
