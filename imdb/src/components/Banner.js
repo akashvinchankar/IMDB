9fa2e7efa56504e09e8f57ff4136f1b8aae5b2cc
akashvinchankar <86399324+akashvinchankar@users.noreply.github.com>
2022-02-12 22:56:34 +0530

    favourites section added

--- imdb/src/components/Banner.js
+++ imdb/src/components/Banner.js
@@ -1,23 +1,32 @@
-import React, { useState, useEffect} from 'react';
-import Image from '../Banner.jpg';
-import axios from 'axios';
-
+import React,{useState,useEffect} from 'react';
+import axios from 'axios'
 function Banner() {
 
-  const [movie, setMovie] = useState({})
+    const [movie, setMovie] = useState({})
+    useEffect(function () {
 
-    useEffect(function(){
-        axios.get("https://api.themoviedb.org/3/trending/movie/week?api_key=5aaccc221c940447d56000631ae7d762").then((res) => {
+        axios.get("https://api.themoviedb.org/3/trending/movie/week?api_key=5540e483a20e0b20354dabc2d66a31c9&page=1").then((res) => {
             console.table(res.data.results)
-            setMovie(res.data.results[0])
-        })
+            setMovie(res.data.results[0]);
+        }
+        )
     },[])
-
-  return <>
-    <div className={`bg-[url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})] h-[40vh] md:h-[60vh] bg-center bg-cover flex items-end`}>
-        <div className='text-xl md:text-3xl text-white font-bold p-4 bg-gray-900 w-full flex justify-center bg-opacity-50'>{movie.title}</div>
-    </div>
-  </>;
+    return <> 
+        <div className=
+        {`bg-[url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})] h-[40vh] md:h-[60vh] 
+        bg-center bg-cover
+        flex items-end  
+    `}>
+        <div className=" text-xl md:text-3xl text-white
+            p-6 
+            bg-gray-900 bg-opacity-50
+            w-full
+            flex justify-center
+        ">
+                {movie?.title}
+                </div>
+        </div>
+    </>;
 }
 
 export default Banner;
--- imdb/src/components/NavBar.js
+++ imdb/src/components/NavBar.js
@@ -1,17 +1,24 @@
-/* eslint-disable jsx-a11y/alt-text */
+// rfce
 import React from 'react';
 import Logo from "../logo.png";
-import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom'
 
 function NavBar() {
     return <>
-        <div className='border pl-12 flex space-x-8 items-center py-4'>
-            <img className='w-[45px] md:w-[60px]' src = {Logo}></img>
-            <Link to="/" className='text-blue-400 font-bold text-xl md:text-3xl'>Movies</Link>
-            <Link to="favourites" className='text-blue-400 font-bold text-xl md:text-3xl'>Favourites</Link>
+        <div className
+        ="border 
+        pl-12 py-4 space-x-8
+         flex  items-center 
+        ">
+            <img className="w-[50px] md:w-[60px]" src={Logo}></img>
+            <Link to="/" className=
+            {`text-blue-400 
+            font-bold 
+            text-xl
+            md:text-3xl`}>Movies</Link>
+            <Link to="favourites" className="text-blue-400 font-bold text-xl md:text-3xl">Favourites</Link>
         </div>
     </>;
 }
 
 export default NavBar;
-
